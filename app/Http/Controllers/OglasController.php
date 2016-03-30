eaef76e916cbdd04e147cbb92b2f37f3071f12ee
azra <azramah6@gmail.com>
2016-03-30 14:43:31 +0200

    Oglasi i poruke

--- app/Http/Controllers/OglasController.php
+++ app/Http/Controllers/OglasController.php
@@ -30,16 +30,6 @@ class OglasController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -99,18 +89,7 @@ class OglasController extends Controller
     {
         return Oglas::find($id);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
+    
     /**
      * Update the specified resource in storage.
      *
