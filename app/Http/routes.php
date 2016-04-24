87cc840bcd7ffbcad1a8eaa3226c3613a04c40a6
Zana Tatar <Zana Tatar>
2016-04-24 21:37:46 +0200

    Gmail

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -56,6 +56,14 @@ Route::get('admini/{id}', 'KorisnikController@dajAdmina');
 /*Verifikacija korisničkog računa servis */
 Route::get('korisnici/verifikuj/{konfirmacijski_kod}', 'KorisnikController@verifikujKorisnika');
 
+
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
 /*
  *
  *
